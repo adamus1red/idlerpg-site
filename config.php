0dc0bf404fc0dc7d1c7dba0e24f2bbb8932ec92d
adamus1red <adamus1red@gmail.com>
2013-06-13 02:13:02 +0100

    fix config file

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 
-$admin_email="adamus1red@gmail.com";
-$admin_nick="GeekSoc";
+$admin_email="you@example.com";
+$admin_nick="YourNick";
 
 // nickname of your bot
 $irpg_bot="IdleBot";
 
 // your game's server
-$irpg_network="irc.geeksoc.org";
+$irpg_network="irc.yournet.org";
 
 // your game's channel
 $irpg_chan="#IdleRPG";
@@ -15,20 +15,20 @@ $irpg_chan="#IdleRPG";
 // full or relative pathname to the DBs:
 
 // character database
-$irpg_db="/home/amcghie/idlerpg/irpg.db";
+$irpg_db="/home/YourNick/idlerpg/irpg.db";
 
 // time modifiers file
-$irpg_mod="/home/amcghie/idlerpg/modifiers.txt";
+$irpg_mod="/home/YourNick/idlerpg/modifiers.txt";
 
 // active quest info file
-$irpg_qfile="/home/amcghie/idlerpg/questinfo.txt";
+$irpg_qfile="/home/YourNick/idlerpg/questinfo.txt";
 
 // image to use for the top logo
 $irpg_logo="idlerpg.png";
 
 // directory in which your site is located from the root directory. my site
 // is http://jotun.ultrazone.org/g7/, so it's "/g7/"
-$BASEURL="/~amcghie/dev/";
+$BASEURL="/g7/";
 
 // width-wise dimension of your map file
 $mapx = 500;
@@ -36,7 +36,7 @@ $mapx = 500;
 // length-wise dimension of your map file
 $mapy = 500;
 
-$net_name = "GeekSoc";
-$net_url = "http://geeksoc.org/";
-$irpg_chan_url = "irc://irc.geeksoc.org/idlerpg";
+$net_name = "MyNetwork";
+$net_url = "http://mynetwork-url.org/";
+$irpg_chan_url = "irc://irc.meynetwork-url.org/idlerpg";
 ?>
