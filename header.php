66d608d95db47d49ef2e94a15ba26b48378ea84e
adamus1red <adamus1red@gmail.com>
2013-06-14 19:21:41 +0200

    Update header.php

--- header.php
+++ header.php
@@ -81,7 +81,17 @@
     </style>
   </head>
   <body>
-    <?php include_once("analytics.php") ?>
+  <?php
+  if ($enable_analytics == True) {
+  echo "<script>\n";
+  echo "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\n";
+  echo "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n";
+  echo "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n";
+  echo "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n";
+  echo "ga('create', ',<?php echo $analytics_tracking_code;?>', '<?php echo $analytics_tracking_domain;?>');\n";
+  echo "ga('send', 'pageview');\n";
+  echo "</script>\n";
+  }?>
     <!-- Idle RPG Logo -->
     
         
