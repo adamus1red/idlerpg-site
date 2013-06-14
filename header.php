d7de687b564b08fee7257b4e72f9d31b5b2d1730
adamus1red <adamus1red@gmail.com>
2013-06-14 23:25:21 +0200

    Update header.php

--- header.php
+++ header.php
@@ -88,7 +88,7 @@ echo "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(
 echo "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\n";
 echo "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\n";
 echo "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');\n";
-echo "ga('create', ',<?php echo $analytics_tracking_code;?>', '<?php echo $analytics_tracking_domain;?>');\n";
+echo "ga('create', ',$analytics_tracking_code', '$analytics_tracking_domain');\n";
 echo "ga('send', 'pageview');\n";
 echo "</script>\n";
 }?>
