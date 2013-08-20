1ff9c175d2615623d4ceb7452157285e3d842609
adamus1red <adamus1red@gmail.com>
2013-08-20 15:01:49 +0100

    fix padding issue

--- header.php
+++ header.php
@@ -23,12 +23,10 @@ Based on a work at http://idlerpg.net/.
     ?>
     <style type="text/css">
         <!-- Global Styles -->
-        <!--body {
+        body {
             padding-top: 60px;
-            padding-bottom: 40px;
-        }-->
+        }
         .container {
-            padding-top: 60px;
             margin: 0 auto;
             max-width: 1000px;
         }
