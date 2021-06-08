16e3244a6e0f569e527650c69752819e835b6c7c
adamus1red <adamus1red@gmail.com>
2021-06-08 22:02:37 +0100

    Replace raw $_GET statements

--- src/json.php
+++ src/json.php
@@ -5,18 +5,19 @@ header("Pragma: no-cache");
 
 include("config.php");
 
-$_GET['player'] = substr($_GET['player'], 0, 30);
-
-/* Determine if a Player was entered. If not, redirect. */
-if ($_GET['player'] == "") header('Location: http://' . $_SERVER['SERVER_NAME'] .
-    ($_SERVER['SERVER_PORT'] != 80 ? ':' . $_SERVER['SERVER_PORT'] : '') . $BASEURL .
-    'players.php');
-
+$player = "";
+if (isset($_GET['player'])) {
+    $player = substr($_GET['player'], 0, 30);
+} else {
+    header('Location: http://' . $_SERVER['SERVER_NAME'] .
+        ($_SERVER['SERVER_PORT'] != 80 ? ':' . $_SERVER['SERVER_PORT'] : '') . $BASEURL .
+        'players.php');
+}
 $file = fopen($irpg_db, "r");
 fgets($file, 1024); // skip top comment
 
 while ($line = fgets($file, 1024)) {
-    if (substr($line, 0, strlen($_GET['player']) + 1) == $_GET['player'] . "\t") {
+    if (substr($line, 0, strlen($player) + 1) == $player . "\t") {
         list(
             $user,, $isadmin, $level, $class, $secs,, $uhost, $online, $idled,
             $x, $y,
@@ -51,34 +52,34 @@ fclose($file);
     "isadmin":<?php print $isadmin ?>,
     "level":<?php print $level ?>,
     "class":"<?php print htmlspecialchars($class, ENT_QUOTES) ?>",
-        "ttl":<?php print $secs ?>,
-        "userhost":"<?php print htmlspecialchars($uhost, ENT_QUOTES) ?>",
-        "online":<?php print $online ?>,
-        "totalidled":<?php print $idled ?>,
-        "pos": {
-            "x":<?php print $x ?>,
-            "y":<?php print $y ?>
-        },
-        "alignment":"<?php print $alignment ?>",
-        "penalties": {
-<?php
-            $sum = 0;
-            foreach ($pen as $key => $val) {
-                echo "            \"$key\":" . intval($val) . ",\n";
-                $sum += intval($val);
-            }
-            echo "        \"total\":$sum\n";
-            ?>
-        },
-        "items":{ 
-<?php
-            $sum = 0;
-            foreach ($item as $key => $val) {
-                echo "            \"$key\":" . intval($val) . ",\n";
-                $sum += intval($val);
-            }
-            echo "        \"total\":$sum\n";
-            ?>
+    "ttl":<?php print $secs ?>,
+    "userhost":"<?php print htmlspecialchars($uhost, ENT_QUOTES) ?>",
+    "online":<?php print $online ?>,
+    "totalidled":<?php print $idled ?>,
+    "pos": {
+        "x":<?php print $x ?>,
+        "y":<?php print $y ?>
+    },
+    "alignment":"<?php print $alignment ?>",
+    "penalties": {
+        <?php
+        $sum = 0;
+        foreach ($pen as $key => $val) {
+            echo "            \"$key\":" . intval($val) . ",\n";
+            $sum += intval($val);
+        }
+        echo "        \"total\":$sum\n";
+        ?>
+    },
+    "items":{
+        <?php
+        $sum = 0;
+        foreach ($item as $key => $val) {
+            echo "            \"$key\":" . intval($val) . ",\n";
+            $sum += intval($val);
+        }
+        echo "        \"total\":$sum\n";
+        ?>
         }
     }
 }
