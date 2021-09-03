29cf7e633be1280ae10fa6b0e7c38a517a616a0a
adamus1red <adamus1red@users.noreply.github.com>
2021-09-03 17:59:52 +0100

    Change <? to <?php
    Closes #35

--- src/header.php
+++ src/header.php
@@ -46,7 +46,7 @@
     <header class="p-3 d-flex bg-dark text-white flex-wrap  py-3 mb-4 border-bottom">
         <div class="container">
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="<? echo $BASEURL ?>" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                <a href="<?php echo $BASEURL ?>" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" fill="currentColor" class="bi bi-cpu" viewBox="0 0 16 16">
                         <path d="M5 0a.5.5 0 0 1 .5.5V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2A2.5 2.5 0 0 1 14 4.5h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14a2.5 2.5 0 0 1-2.5 2.5v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14A2.5 2.5 0 0 1 2 11.5H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2A2.5 2.5 0 0 1 4.5 2V.5A.5.5 0 0 1 5 0zm-.5 3A1.5 1.5 0 0 0 3 4.5v7A1.5 1.5 0 0 0 4.5 13h7a1.5 1.5 0 0 0 1.5-1.5v-7A1.5 1.5 0 0 0 11.5 3h-7zM5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3zM6.5 6a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z" />
                     </svg>
@@ -74,13 +74,13 @@
                                                             echo " active";
                                                         } ?>" href="<?php echo $BASEURL; ?>worldmap.php">World Map</a>
                     </li>
-                    <? if (isset($site_pages) && count($site_pages) > 0) {
+                    <?php if (isset($site_pages) && count($site_pages) > 0) {
                         foreach ($site_pages as $key => $value) {
                     ?>
                             <li class="nav-item">
                                 <a class="nav-link text-white <?php if ($BASEURL . $value == $_SERVER['PHP_SELF']) {
                                                                     echo " active";
-                                                                } ?>" href="<?php echo $BASEURL . $value; ?>"><? echo $key; ?></a>
+                                                                } ?>" href="<?php echo $BASEURL . $value; ?>"><?php echo $key; ?></a>
                             </li>
                     <?
                         }
